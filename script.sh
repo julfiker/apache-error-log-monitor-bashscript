f4c5b14af7fa8d7597596b3752c4c2b6c3f2e972
Md. Julfiker ali <infro.jewel@gmail.com>
2018-05-28 18:57:02 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,6 @@ if [ ! -f $p ]; then
 fi
 
 error_log() {
-  url="https://hooks.slack.com/services/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"
   while inotifywait -e modify $p; do
     line=$(tail -n1 $p | cut -c 81-);
     echo "$line"    
